90ef0245b706f89f2f45a2097831fa021ee909b6
Georgi Lambov <g.lambov@zariba.com>
2013-04-18 11:03:36 +0300

    Code is updated to download iAd reporst from iTunes connect.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ iAdDownload
 
 This is a function that can be used to download iad reports from iTunes Connect.
 
+## Getting Started
+
 It requires that the python requests library is installed:
 
 *  pip install requests
@@ -14,5 +16,14 @@ There is an optional argument for publisherId which I recommend you store for th
 future since it will speed up the script and because the scraping to find the
 value for publisherId is likely to be fragile.
 
-There is currently no command line operation on this script.
+## How to use it
 
+python iaddownload.py login password publisherId
+
+## Changes
+
+### Version 1.1.1
+
+* Update code to download iAd reports from iTunes connect
+* Add support for command line exectution
+* Retrieve ProviderId from iTunes doesn't work
--- iaddownload.py
+++ iaddownload.py
@@ -1,5 +1,6 @@
 import requests
-import re, os, datetime
+import re, os, datetime, sys
+import urllib
 
 url_base = 'https://itunesconnect.apple.com%s'
 signin_url = url_base % '/WebObjects/iTunesConnect.woa'
@@ -10,21 +11,24 @@ def getPublisherID(session):
     publisher ID is a number that is buried deep... lets hope
     this code doesn't break often
     """
+    #FIXME: doesn't work at the moment
     headers = {'Content-type': 'text/x-gwt-rpc; charset=UTF-8'}
-    iad_service_url= 'https://iad.apple.com/itcportal/service/startup?requestId=GUEST_1331628761939_0__1331628761950__null'
+    iad_service_url = 'https://iad.apple.com/itcportal/service/startup?requestId=GUEST_1331628761939_0__1331628761950__null'
     body = '5|0|4|https://iad.apple.com/itcportal/itcportal/|E8DB97D87973D76A7C9096DCF8A83BB5|com.qwapi.portal.itc.client.rpc.IITCStartupService|getStartupData|1|2|3|4|0|'
     r = session.post(iad_service_url, data=body, headers=headers)
     endpos = r.text.find('com.qwapi.portal.client.rpc.dto.UserDTO')
-    endpos = r.text.find('"', endpos-4)
-    startpos = r.text.rfind('"', endpos-20, endpos-4)
-    pubID = r.text[startpos+1:endpos]
+    endpos = r.text.find('"', endpos - 4)
+    startpos = r.text.rfind('"', endpos - 20, endpos - 4)
+    pubID = r.text[startpos + 1:endpos]
     if not int(pubID):
         raise Exception("Failed to get publisher ID")
     return pubID
 
 
 def downloadiAdFiles(appleId, password, publisherId=None, outputDirectory='.', daysToDownload=14, outputFormat='iAd_D_%m-%d-%Y.txt'):
-
+    """
+    Download iAd report files.
+    """
     session = requests.session()  #session used to maintain cookies etc.
 
     #get signin page to find url for posting signin credentials
@@ -34,16 +38,19 @@ def downloadiAdFiles(appleId, password, publisherId=None, outputDirectory='.', d
     #login to ITC
     params = {'theAccountName':appleId,
             'theAccountPW':password,
-            '1.Continue.x':'10',
-            '1.Continue.y':'10',
+            '1.Continue.x':'0',
+            '1.Continue.y':'0',
             'theAuxValue':''}
-    r = session.post(url_base % match.group(1), params=params, headers={'Content-Length':'0'})
-    r = session.get('https://iad.apple.com/itcportal')
-
+    
+    headers = {'Content-Length':'0',
+               'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:16.0) Gecko/20100101 Firefox/16.0'}
+    
+    r = session.post(url_base % match.group(1), params=params, headers={})
+    
     if publisherId is None:
         publisherId = getPublisherID(session)
 
-    reportDates = [datetime.date.today() - datetime.timedelta(i + 1 ) for i in range(daysToDownload)]
+    reportDates = [datetime.date.today() - datetime.timedelta(i + 1) for i in range(daysToDownload)]
 
     filenames = []
     for downloadReportDate in reportDates:
@@ -56,13 +63,23 @@ def downloadiAdFiles(appleId, password, publisherId=None, outputDirectory='.', d
             'dashboardType':'publisher',
             'publisherId':publisherId,
             'dateRange':'customDateRange',
-            'searchTerms':'Search Apps',
+            'searchTerms':'Search%20Apps',
             'adminFlag':'false',
             'fromDate':dateString,
             'toDate':dateString,
             'dataType':'byName'
         }
-        r = session.get(download_csv_url, params=params)
+        
+        cookieParams = {'s_sq' : 'applesuperglobal%3D%2526pid%253DiTC%252520Home%2526pidt%253D1%2526oid%253Dhttps%25253A%25252F%25252Fiad.apple.com%25252Fitcportal%2526ot%253DA',
+                        's_cc' : 'true',
+                        'ds01' : r.cookies['ds01'],
+                        'myacinfo' : r.cookies['myacinfo']}
+        
+        headers = {'Cookie' : urllib.urlencode(cookieParams).replace("&", "; "),
+                   'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:16.0) Gecko/20100101 Firefox/16.0'}
+        
+        r = session.get(download_csv_url, params=params, headers=headers)
+        
         if r.status_code != 200:
             raise Exception("Script failed to dowload - check login credentials & publisher ID")
 
@@ -72,3 +89,20 @@ def downloadiAdFiles(appleId, password, publisherId=None, outputDirectory='.', d
 
     return filenames
 
+
+def main():
+    '''
+    Test iAd reports
+    '''
+    argv = sys.argv[1:]
+    argvLength = len(argv)
+
+    appleId = argv[0] if argvLength > 0 else "" #provide account
+    password = argv[1] if argvLength > 1 else "" #provide password
+    publisherId = argv[2] if argvLength > 2 else 0 #provide publisherId
+    
+    downloadiAdFiles(appleId, password, publisherId)    
+
+
+if __name__ == "__main__":
+    main()
